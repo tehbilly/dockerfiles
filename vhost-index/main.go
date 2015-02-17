dba804e8103ebfb6dac7c09860e1fb485784e3a9
William McGann <contact@WilliamMcGann.com>
2015-02-17 16:31:25 -0500

    Adding basic authentication. Controlled via env.

--- vhost-index/main.go
+++ vhost-index/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
 	"os"
+	"strings"
 
+	"github.com/codegangsta/negroni"
 	"github.com/fsouza/go-dockerclient"
 	"github.com/gorilla/mux"
 )
@@ -30,8 +34,36 @@ func init() {
 }
 
 func main() {
+	n := negroni.New()
+	n.Use(negroni.NewStatic(http.Dir("site")))
+
+	// Set up a Basic Authentication handler unless explicitly disabled
+	if os.Getenv("AUTH_OFF") != strings.ToLower("true") {
+		ah := NewAuthHandler()
+		var user string
+		if os.Getenv("AUTH_USER") != "" {
+			user = os.Getenv("AUTH_USER")
+		} else {
+			user = "vadmin"
+		}
+		var pass string
+		if os.Getenv("AUTH_PASS") != "" {
+			pass = os.Getenv("AUTH_PASS")
+		} else {
+			src := "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ1234567890"
+			pwb := make([]byte, 12)
+			for i := range pwb {
+				pwb[i] = src[rand.Int()%len(src)]
+			}
+			pass = string(pwb)
+		}
+		fmt.Printf("Auth User: %s\nAuth Pass: %s\n", user, pass)
+		ah.AddUser(user, pass)
+		n.Use(ah)
+	}
+
+	// Gotta have a router
 	r := mux.NewRouter()
-	r.NotFoundHandler = http.FileServer(http.Dir("site"))
 	r.HandleFunc("/containers/list", ContainerList)
 	r.HandleFunc("/containers/{id}/info", ContainerInfo)
 	r.HandleFunc("/containers/{id}/start", ContainerStart)
@@ -41,14 +73,50 @@ func main() {
 	r.HandleFunc("/images/list", ImageList)
 	r.HandleFunc("/images/{id}/info", ImageInfo)
 	r.HandleFunc("/dockerinfo", DockerServerInfo)
+	n.UseHandler(r)
 
-	fmt.Println("Starting http server...")
-	err := http.ListenAndServe(":3000", r)
+	// So `docker logs` shows something
+	fmt.Println("Starting VHost server.")
+	err := http.ListenAndServe(":3000", n)
 
 	if err != nil {
 		fmt.Println("Unable to start http server:", err)
 		os.Exit(1)
 	}
+}
+
+// Simple authentication for a simple application.
+// AuthHandler contains users we trust.
+type AuthHandler struct {
+	// A string of users that are authorized to access the application.
+	// Users[username] = base64.StdEncoding.EncodeToString([]byte(username+":"+password))
+	Users map[string]string
+}
+
+// Create a new (empty) AuthHandler
+func NewAuthHandler() *AuthHandler {
+	a := &AuthHandler{
+		Users: make(map[string]string),
+	}
+	return a
+}
+
+// Add a user that's allowed to log in
+func (a *AuthHandler) AddUser(name, pass string) {
+	a.Users[name] = base64.StdEncoding.EncodeToString([]byte(name + ":" + pass))
+}
+
+func (a *AuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	h := r.Header.Get("Authorization")
+	for _, as := range a.Users {
+		if "Basic "+as == h {
+			next(w, r)
+			return
+		}
+	}
+	// Still here? Let's go ahead and prompt for authorization
+	w.Header().Set("WWW-Authenticate", "Basic realm=\"VHost Index\"")
+	http.Error(w, "Not Authorized", http.StatusUnauthorized)
 }
 
 func DockerServerInfo(w http.ResponseWriter, r *http.Request) {
